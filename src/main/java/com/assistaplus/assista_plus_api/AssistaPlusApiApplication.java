eb855fd3db15367bb18ca046ac935f34c3cdec7b
RAFAEL DE OLIVEIRA LIMA <rafael.olima11@senacsp.edu.br>
2026-09-11 09:46:31 -0300

    chore: inicializa projeto Assista+

---
+++ src/main/java/com/assistaplus/assista_plus_api/AssistaPlusApiApplication.java
@@ -0,0 +1,13 @@
+package com.assistaplus.assista_plus_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssistaPlusApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssistaPlusApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assistaplus/assista_plus_api/AssistaPlusApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assistaplus.assista_plus_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssistaPlusApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
